24baa13cc6b170793ac9a3204e51eba10425aec6
Dan Philibin <dan@danphilibin.com>
2022-07-18 19:54:15 -0700

    move auth into its own function and remove 'restart' button

---
+++ src/auth.ts
@@ -0,0 +1,75 @@
+import { ctx, io } from "@interval/sdk";
+import spotifyApi from "./spotify";
+import { spotifyScopes } from "./util";
+
+// store this in memory to reuse between transactions, as long as the server doesn't restart
+export let accessToken: string | null = null;
+
+function checkRequiredKeys() {
+  const requiredKeys = [
+    "SPOTIFY_CLIENT_ID",
+    "SPOTIFY_CLIENT_SECRET",
+    "INTERVAL_KEY",
+  ];
+
+  for (const key of requiredKeys) {
+    if (!process.env[key]) {
+      throw new Error(`Missing required environment variable: ${key}`);
+    }
+  }
+}
+
+async function checkAuth() {
+  spotifyApi.setAccessToken(accessToken);
+  await spotifyApi.getMe();
+}
+
+export async function requireSpotifyAuth() {
+  checkRequiredKeys();
+
+  const redirectUri = `https://interval.com/dashboard/${
+    ctx.organization.slug
+  }/${ctx.environment === "development" ? "develop/actions" : "actions"}/${
+    ctx.action.slug
+  }`;
+
+  // IMPORTANT: this URI must be added to the Spotify developer dashboard
+  spotifyApi.setRedirectURI(redirectUri);
+
+  try {
+    if (!accessToken && !ctx.params.code) {
+      await beginAuth(redirectUri);
+      return;
+    }
+
+    if (ctx.params.code) {
+      // returning from the OAuth flow; request an access token
+      const tokens = await spotifyApi.authorizationCodeGrant(
+        String(ctx.params.code)
+      );
+
+      accessToken = tokens.body.access_token;
+    }
+
+    await checkAuth();
+  } catch (error) {
+    console.error(error);
+
+    // start over
+    await beginAuth(redirectUri);
+  }
+}
+
+async function beginAuth(redirectUri: string) {
+  // Generate a URL for the user to visit to authorize the app
+  const params = new URLSearchParams({
+    response_type: "code",
+    client_id: process.env.SPOTIFY_CLIENT_ID,
+    redirect_uri: redirectUri,
+    scope: spotifyScopes.join(" "),
+  });
+
+  await io.display.link("Click here to authorize Spotify", {
+    href: `https://accounts.spotify.com/authorize?${params.toString()}`,
+  });
+}
--- src/index.ts
+++ src/index.ts
@@ -1,96 +1,19 @@
 import "dotenv/config";
 import Interval, { ctx, io } from "@interval/sdk";
-import SpotifyWebApi from "spotify-web-api-node";
 import {
   getDateString,
   monthNames,
   sleep,
   splitArrayIntoBatches,
-  spotifyScopes,
 } from "./util";
-
-// store this in memory to reuse between transactions, as long as the server doesn't restart
-let accessToken: string | null = null;
-
-const spotifyApi = new SpotifyWebApi({
-  clientId: process.env.SPOTIFY_CLIENT_ID,
-  clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-});
-
-async function checkAuth() {
-  spotifyApi.setAccessToken(accessToken);
-  await spotifyApi.getMe();
-}
-
-function checkRequiredKeys() {
-  if (!("SPOTIFY_CLIENT_ID" in process.env)) {
-    throw new Error("SPOTIFY_CLIENT_ID is not set");
-  }
-  if (!("SPOTIFY_CLIENT_SECRET" in process.env)) {
-    throw new Error("SPOTIFY_CLIENT_SECRET is not set");
-  }
-  if (!("INTERVAL_KEY" in process.env)) {
-    throw new Error("INTERVAL_KEY is not set");
-  }
-  if (!("INTERVAL_ORG_SLUG" in process.env)) {
-    throw new Error("INTERVAL_ORG_SLUG is not set");
-  }
-}
+import { requireSpotifyAuth } from "./auth";
+import spotifyApi from "./spotify";
 
 const interval = new Interval({
   apiKey: process.env.INTERVAL_KEY,
   actions: {
     monthly_likes_playlist: async () => {
-      const actionSlug = "monthly_likes_playlist";
-
-      checkRequiredKeys();
-
-      // important! the slug below must match the slug of this action, including your organization:
-      const redirectUri = `https://interval.com/dashboard/${process.env.INTERVAL_ORG_SLUG}/develop/actions/${actionSlug}`;
-
-      spotifyApi.setRedirectURI(redirectUri);
-
-      try {
-        if (!accessToken && !ctx.params.code) {
-          // Generate a URL for the user to visit to authorize the app
-          const params = new URLSearchParams({
-            response_type: "code",
-            client_id: process.env.SPOTIFY_CLIENT_ID,
-            redirect_uri: redirectUri,
-            scope: spotifyScopes.join(" "),
-          });
-
-          await io.display.link("Click here to authorize Spotify", {
-            href: `https://accounts.spotify.com/authorize?${params.toString()}`,
-          });
-
-          return;
-        }
-
-        if (ctx.params.code) {
-          // returning from the OAuth flow; request an access token
-          const tokens = await spotifyApi.authorizationCodeGrant(
-            String(ctx.params.code)
-          );
-
-          accessToken = tokens.body.access_token;
-        }
-
-        await checkAuth();
-      } catch (error) {
-        console.error(error);
-
-        await io.group([
-          io.display.markdown(
-            "The auth state has expired, please restart the action:"
-          ),
-          io.display.link("Restart action", {
-            href: redirectUri,
-            // linking to the same action currently does not work
-            // action: actionSlug,
-          }),
-        ]);
-      }
+      await requireSpotifyAuth();
 
       const date = await io.experimental.date(
         "Choose a month to generate a playlist for:"
