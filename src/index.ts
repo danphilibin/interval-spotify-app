2a6e0c14da7e0221dc6fe6aa88c23fd6596eb623
Dan Philibin <dan@danphilibin.com>
2023-04-27 05:19:46 +0000

    WIP

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
 import "dotenv/config";
 import Interval from "@interval/sdk";
 import path from "path";
+import express from "express";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Heartbeat");
+});
+
+app.listen(port, () => console.log(`Spotify app listening on port ${port}!`));
 
 const interval = new Interval({
   apiKey: process.env.INTERVAL_KEY,
