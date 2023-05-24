f3d900809cf3577ac6dafe37982681391ae27bb4
Dan Philibin <dan@danphilibin.com>
2023-05-24 14:45:40 +0000

    add ping and logging

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ import "dotenv/config";
 import Interval from "@interval/sdk";
 import path from "path";
 import express from "express";
+import consoleStamp from "console-stamp";
+
+consoleStamp(console);
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -15,6 +18,8 @@ app.listen(port, () => console.log(`Spotify app listening on port ${port}!`));
 const interval = new Interval({
   apiKey: process.env.INTERVAL_KEY,
   routesDirectory: path.join(__dirname, "routes"),
+  pingIntervalMs: 30_000,
+  pingTimeoutMs: 5_000,
 });
 
 interval.listen();
